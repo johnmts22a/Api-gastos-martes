3f95766851e0185f4f078acdde0f76860c9a8140
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-10-01 20:52:36 -0500

    Se hicieron los métodos de guardar y buscar en usuarios y gastos

--- app/api/routes/endpoints.py
+++ app/api/routes/endpoints.py
@@ -4,6 +4,8 @@ from typing import List
 from fastapi.params import Depends #Utilizar dependencias del api para comunicación interna.
 from app.api.DTO.dtos import UsuarioDTOPeticion, UsuarioDTORespuesta
 from app.api.models.tablasSQL import Usuario
+from app.api.DTO.dtos import GastoDTOPeticion, GastoDTORespuesta
+from app.api.models.tablasSQL import Gasto
 from app.database.configuration import SessionLocal,engine
 
 rutas = APIRouter()
@@ -14,8 +16,74 @@ def conectarConBD():
         yield basedatos #Activar la base de datos
            
     except Exception as error:
-        basedatos.rollback()
+        basedatos.rollback() #Si la base de datos falló no hacer nada.
         raise error
     
     finally:
         basedatos.close()
+        
+#Construyendo nuestros servicios.
+
+#Cada servicio (operación o transacción en BD) debe programarse como una función.
+
+@rutas.post("/usuario",response_model = Usuario, summary="Registrar un usuario en la base de datos")
+
+def guardarUsuario(datosUsuario:UsuarioDTOPeticion, database:Session=Depends(conectarConBD)): #datos y conexión con la base de datos respectivamente.
+    try:
+        usuario = Usuario(
+            nombres = datosUsuario.nombres,
+            fechaNacimiento = datosUsuario.fechaNacimiento,
+            ubicacion = datosUsuario.ubicacion,
+            metaAhorro = datosUsuario.metaAhorro
+        )
+        
+        #Dando órdenes a la base de datos:
+        
+        database.add(usuario) #Agregar el usuario a la base de datos.
+        database.commit() 
+        database.refresh(usuario) #Refrescar la base de datos.
+        return usuario
+        
+    except Exception as error:
+        database.rollback() #Si la base de datos falló no hacer nada. 
+        raise HTTPException(status_code=400,detail= f"Tenemos un problema {error}")
+    
+@rutas.get("/usuario",response_model = List[UsuarioDTORespuesta],summary="Buscar todos los usuario en la base de datos")
+    
+def buscarUsuario(database:Session=Depends(conectarConBD)):
+    try:
+        usuarios = database.query(Usuario).all
+        return usuarios
+        
+    except Exception as error:
+        database.rollback() #Si la base de datos falló no hacer nada. 
+        raise HTTPException(status_code=400,detail= f"No se puede buscar los usuario {error}")
+    
+@rutas.post("/gasto",response_model= Gasto, summary= "Registrar un gasto en la base de datos")
+        
+def guardarGasto(datosGasto:GastoDTOPeticion, database:Session=Depends(conectarConBD)):
+    try:
+       gasto = Gasto(
+           descripcion = datosGasto.descripcion,
+           categoria = datosGasto.categoria,
+           valor = datosGasto.valor,
+           fecha = datosGasto.fecha
+       )
+       
+       database.add(gasto) #Agregar el usuario a la base de datos.
+       database.commit() 
+       database.refresh(gasto) #Refrescar la base de datos.
+       return gasto
+       
+    except Exception as error:
+        database.rollback() #Si la base de datos falló no hacer nada. 
+        raise HTTPException(status_code=400,detail= f"Tenemos un problema {error}")
+    
+def buscarGasto(database:Session=Depends(conectarConBD)):
+    try:
+        gastos = database.query(Gasto).all
+        return gastos
+     
+    except Exception as error:
+        database.rollback() #Si la base de datos falló no hacer nada. 
+        raise HTTPException(status_code=400,detail= f"No se puede buscar los usuario {error}")
